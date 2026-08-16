d1775db4f7fa6219a7be871c373f03e5f72f2431
DAVID J FOX <davidjfox998@gmail.com>
2026-08-16 16:48:00 -0700

    Remove leanOptions from package configuration

--- lakefile.lean
+++ lakefile.lean
@@ -2,14 +2,9 @@ import Lake
 open Lake DSL
 
 package «beal-conjecture» where
-  leanOptions := #[
-    ⟨`pp.unicode.fun, true⟩,
-    ⟨`autoImplicit, false⟩
-  ]
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.12.0"
 
 @[default_target]
 lean_lib Beal where
-  -- no globs needed, uses Beal.lean as entry
