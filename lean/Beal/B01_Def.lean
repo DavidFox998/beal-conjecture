cc5bcfa7f9ba4fd6b919434c85392b6ab077311c
DAVID J FOX <davidjfox998@gmail.com>
2026-08-17 11:53:33 -0700

    Define BealConjecture proposition in Lean

--- lean/Beal/B01_Def.lean
+++ lean/Beal/B01_Def.lean
@@ -5,3 +5,8 @@ def IsBealSolution (A B C x y z : Nat) : Prop :=
   2 < x ∧ 2 < y ∧ 2 < z ∧
   A ^ x + B ^ y = C ^ z ∧
   Nat.gcd A (Nat.gcd B C) = 1
+
+def _root_.BealConjecture : Prop :=
+  ∀ A B C x y z, IsBealSolution A B C x y z → False
+
+def _root_.Beal.BealConjecture : Prop := _root_.BealConjecture
