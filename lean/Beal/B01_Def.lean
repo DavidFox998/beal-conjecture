ac8dcaf625bc8a2abcee2bbbbe7a023d32a47525
DAVID J FOX <davidjfox998@gmail.com>
2026-08-17 00:27:16 -0700

    Refactor IsBealSolution and update conditions

--- lean/Beal/B01_Def.lean
+++ lean/Beal/B01_Def.lean
@@ -1,15 +1,7 @@
-import Mathlib.Data.Nat.GCD.Basic
 import Mathlib.Data.Nat.Prime.Basic
 
 def IsBealSolution (A B C x y z : Nat) : Prop :=
-  x > 2 ∧ y > 2 ∧ z > 2 ∧ 0 < A ∧ 0 < B ∧ 0 < C ∧
-  A ^ x + B ^ y = C ^ z ∧ Nat.Coprime A B ∧ Nat.Coprime B C ∧ Nat.Coprime A C
-
-def BealConjecture : Prop :=
-  ¬ ∃ A B C x y z, IsBealSolution A B C x y z
-
-theorem beal_trivial_no_zero : ¬ IsBealSolution 0 B C x y z := by
-  simp [IsBealSolution]
-
-#print axioms IsBealSolution
--- should print only propext, Quot.sound
+  0 < A ∧ 0 < B ∧ 0 < C ∧
+  2 < x ∧ 2 < y ∧ 2 < z ∧
+  A ^ x + B ^ y = C ^ z ∧
+  Nat.gcd A (Nat.gcd B C) = 1
