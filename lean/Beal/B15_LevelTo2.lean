992dbd798b372f95c7287ea6e1e860929e42b3b4
DAVID J FOX <davidjfox998@gmail.com>
2026-08-17 23:16:15 -0700

    Simplify RibetLowersToLevel2 and BealFinalContradiction

--- lean/Beal/B15_LevelTo2.lean
+++ lean/Beal/B15_LevelTo2.lean
@@ -1,29 +1,13 @@
 import Beal.B14_FreyConductor
 
-set_option linter.unusedVariables false
-
 namespace BealLevelTo2
 
-open BealRibetReal
-open BealFreyConductor
+def RibetLowersToLevel2 : Prop := True
 
-def RibetLowersToLevel2 : Prop :=
-  forall A B C x y z p N M,
-    IsBealSolution A B C x y z ->
-    CanLowerLevel N p ->
-    M = N / p ->
-    M = 2 -> True
+theorem ribet_lowers_to_2_trivial : RibetLowersToLevel2 := trivial
 
-theorem ribet_lowers_to_2_trivial : RibetLowersToLevel2 :=
-  fun _ _ _ _ _ _ _ _ _ _ _ => trivial
+def BealFinalContradiction : Prop := True
 
-def BealFinalContradiction : Prop :=
-  RibetLowersToLevel2 -> Not S2NewformAtLevel2 -> True
-
-theorem beal_final_trivial : BealFinalContradiction :=
-  fun _ _ => trivial
-
-#print axioms ribet_lowers_to_2_trivial
-#print axioms beal_final_trivial
+theorem beal_final_trivial : BealFinalContradiction := trivial
 
 end BealLevelTo2
