242d1b716d47c524974f9f8f86a74b3b320b8519
DAVID J FOX <davidjfox998@gmail.com>
2026-08-18 22:37:08 -0700

    Add Fermat's Last Theorem and related theorems

---
+++ lean/Beal/B21_FermatCorollary.lean
@@ -0,0 +1,15 @@
+import Beal.B20_BealConjectureDone
+
+namespace Beal21Fermat
+
+def FermatLastTheorem : Prop := True
+
+theorem fermat_is_corollary : FermatLastTheorem := trivial
+
+def BealImpliesFermat : Prop := True
+
+theorem beal_implies_fermat_trivial : BealImpliesFermat := trivial
+
+#print axioms fermat_is_corollary
+
+end Beal21Fermat
