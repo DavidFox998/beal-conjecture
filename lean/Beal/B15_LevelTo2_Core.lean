6065773df881d779ed19d8d6fd164b647e2b4c54
DAVID J FOX <davidjfox998@gmail.com>
2026-08-19 23:53:08 -0700

    Remove CanLowerLevelExists definition and print axioms

--- lean/Beal/B15_LevelTo2_Core.lean
+++ lean/Beal/B15_LevelTo2_Core.lean
@@ -1,8 +1,4 @@
 def CanLowerLevelCore (N p M : Nat) : Prop :=
   M * p = N
 
-def CanLowerLevelExists (N p : Nat) : Prop :=
-  ∃ M, M = N / p ∧ N % p = 0 ∧ CanLowerLevelCore N p M
-
 #print axioms CanLowerLevelCore
-#print axioms CanLowerLevelExists
